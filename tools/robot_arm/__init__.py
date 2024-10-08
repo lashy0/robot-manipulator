9c99adf1fb92f4f2ced8c38cf575b7679a508b00
Danya Alekseev <lashy.danya.work@gmail.com>
2024-10-08 04:38:38 +0300

    update robot_arm

--- tools/robot_arm/__init__.py
+++ tools/robot_arm/__init__.py
@@ -1,7 +1,9 @@
 from .model import ROBOT_CHAIN, START_ANGLE_POSITION
 from . import utils
+from .commands import set_angle, set_manipulator, set_grip
 
 __all__ = [
     'ROBOT_CHAIN', 'START_ANGLE_POSITION',
-    'utils'
+    'utils',
+    'set_angle', 'set_manipulator', 'set_grip'
 ]
